a25cee61494b9c57063e3a59b217c18a94db6804
Lygiasbotelho <lygiasi@gmail.com>
2021-02-28 20:30:31 -0300

    Add modal

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { GetServerSideProps } from "next";
 
 import { CompletedChallenges } from "../components/CompleteChallenges";
 import { Countdown } from "../components/Countdown";
@@ -9,48 +10,54 @@ import { ChallengeBox } from "../components/ChallengeBox";
 import styles from "../styles/pages/Home.module.css";
 import { CountDownProvider } from "../contexts/CountdownContext";
 
+import { ChallengesProvider } from "../contexts/ChallengesContext";
 
-export default function Home(props) {
+interface HomeProps {
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
+}
 
- // console.log("Props:", props);
+export default function Home(props: HomeProps) {
+  console.log("Props:", props);
 
-  
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Início | move.it</title>
-      </Head>
-      <ExperienceBar />
+    <ChallengesProvider 
+    level={props.level} 
+    currentExperience={props.currentExperience}
+    challegensCompleted={props.challengesCompleted}
+    >
+      <div className={styles.container}>
+        <Head>
+          <title>Início | move.it</title>
+        </Head>
+        <ExperienceBar />
 
-      <CountDownProvider>      
-        <section>
-          <div>
-            <Profile />
-            <CompletedChallenges />
-            <Countdown />
-          </div>
-          <div>
-            <ChallengeBox />
-          </div>
-        </section>
-      </CountDownProvider>
-    </div>
+        <CountDownProvider>
+          <section>
+            <div>
+              <Profile />
+              <CompletedChallenges />
+              <Countdown />
+            </div>
+            <div>
+              <ChallengeBox />
+            </div>
+          </section>
+        </CountDownProvider>
+      </div>
+    </ChallengesProvider>
   );
 }
 
-
-export const getServerSideProps = async () => {
-
-  //chamada API
-  const user = {
-    level: 1,
-    currentExperience: 50,
-    challengesCompleted: 2,    
-  }
-
-  console.log("User:", user);
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { level, currentExperience, challengesCompleted } = ctx.req.cookies;
 
   return {
-    props: user
-  }
-}
+    props: {
+      level: Number(level),
+      currentExperience: Number( currentExperience),
+      challengesCompleted: Number(challengesCompleted),
+    },
+  };
+};
